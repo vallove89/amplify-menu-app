fc30036deec1eeb6c0779cb7e6e54d4a7cd6f1c8
vallove89 <vallove89@outlook.com>
2024-12-30 19:32:12 -0600

    Added the schema for the menu

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { describe } from "node:test";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -12,20 +13,119 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.owner()]),
+  Dish: a.model({
+    id: a.id().required(),
+    userId: a.id().required(),
+    name: a.string().required(),
+    description: a.string(),
+    price: a.float(),
+    imageUrl: a.string(),
+    submenus: a.hasMany("SubmenuDish","dishId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
   Drink: a
   .model({
     id: a.id().required(),
     userId: a.id().required(),
     name: a.string().required(),
     description: a.string(),
-    imageUrl: a.string(),
     category: a.boolean().default(false),
-    price: a.float()
+    price: a.float(),
+    imageUrl: a.string(),
+    submenus: a.hasMany("SubmenuDrink","drinkId")
   })
   .authorization((allow) => [
     allow.publicApiKey().to(['read']),
     allow.owner(),
-  ])
+  ]),
+  Side: a
+  .model({
+    id: a.id().required(),
+    userId: a.id().required(),
+    name: a.string().required(),
+    description: a.string(),
+    price: a.float(),
+    imageUrl: a.string(),
+    submenus: a.hasMany("SubmenuSide","sideId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  Submenu: a
+  .model({
+    id: a.id().required(),
+    userId: a.id().required(),
+    title: a.string().required(),
+    description: a.string(),
+    imageUrl: a.string(),
+    menus: a.hasMany("MenuSubmenu","submenuId"),
+    dishs: a.hasMany("SubmenuDish","submenuId"),
+    sides: a.hasMany("SubmenuSide","submenuId"),
+    drinks: a.hasMany("SubmenuDrink","submenuId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  Menu: a
+  .model({
+    id: a.id().required(),
+    userId: a.id().required(),
+    title: a.string().required(),
+    submenus: a.hasMany("MenuSubmenu", "menuId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  MenuSubmenu: a
+  .model({
+    menuId: a.id().required(),
+    submenuId: a.id().required(),
+    menu: a.belongsTo("Menu","menuId"),
+    submenu: a.belongsTo("Submenu","submenuId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  SubmenuDish: a
+  .model({
+    submenuId: a.id().required(),
+    dishId: a.id().required(),
+    submenu: a.belongsTo("Submenu","submenuId"),
+    dish: a.belongsTo("Dish","dishId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  SubmenuSide: a
+  .model({
+    submenuId: a.id().required(),
+    sideId: a.id().required(),
+    submenu: a.belongsTo("Submenu","submenuId"),
+    side: a.belongsTo("Side","sideId")
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
+  SubmenuDrink: a
+  .model({
+    submenuId: a.id().required(),
+    drinkId: a.id().required(),
+    submenu: a.belongsTo("Submenu","submenuId"),
+    drink: a.belongsTo("Drink","drinkId")
+  })  
+  .authorization((allow) => [
+    allow.publicApiKey().to(['read']),
+    allow.owner(),
+  ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
