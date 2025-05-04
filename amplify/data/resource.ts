95f0ddc784e5ba5c02224191201b45716a157de6
vallove89 <vallove89@outlook.com>
2025-05-04 13:10:30 -0500

    change the belongTo field of Item from dishId to itemId

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -87,11 +87,11 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    //defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: "userPool",
     // API Key is used for a.allow.public() rules
-    //apiKeyAuthorizationMode: {
-    //  expiresInDays: 30,
-    //},
+      apiKeyAuthorizationMode: {
+        expiresInDays: 30,
+    },
   },
 });
 
